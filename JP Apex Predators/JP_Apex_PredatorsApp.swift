e313acd55a34113bdca752ee85ced0eb8b4a4e7a
Nuno Mendonça <nunomendonca95@gmail.com>
2023-06-02 18:57:18 +0100

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Nuno Mendonça on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
